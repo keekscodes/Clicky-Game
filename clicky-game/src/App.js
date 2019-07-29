2ad4002be1b04dd67fa304a6b221da3a3ce4a7ba
Kimberly <kamueller2@gmail.com>
2019-07-29 16:09:15 -0400

    functions and state

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 import Cards from './components/Cards';
 import Navbar from './components/Navbar';
@@ -7,30 +7,84 @@ import Wrapper from './components/Wrapper';
 import character from './character.json'
 
 
-
+function random(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 
 class App extends Component {
   // Setting this.state.cards to the cards json array
   state = {
-    character,
     score: 0,
-    topScore: 0
+    topScore: 0,
+    selected: [],
+    gameOver: false,
+    countdown: '',
   };
 
+  mixCards = () => {
+    let shuffled = random(character);
+    this.setState({chars: shuffled});
+  }
+  handleClick = (name) => {
+    if (!this.state.gameover) {
+      if (this.state.selected.indexOf(name) === -1) {
+        // this.increment();
+        this.setState({ selected: [...this.state.selected, name] });
+      } else {
+        this.setState({ msg: 'GAME OVER', gameOver: true })
+        this.reset();
+        setTimeout(() => {
+          this.setState({ countdown: 3 });
+        }, 1000)
+        setTimeout(() => {
+          this.setState({ countdown: 2 });
+        }, 2000)
+        setTimeout(() => {
+          this.setState({ countdown: 1 });
+        }, 3000)
+      }
+    } else {
+      this.setState({ 
+        msg: 'Click any character to begin', 
+        selected: [],
+        score: 0,
+        gameover: false
+      });
+    }
+  }
 
-// Map over this.state.cards and render a cardCard component for each card object
+// Map over this.state.cards and render a card component for each card object
  render() {
     return (
       <Wrapper>
         <Navbar score={this.state.score} topScore={this.state.topScore}>Clicky Game</Navbar>
-        {this.state.character.map(character => (
-          <Cards
+        <div className="container">
+         <div className="row">
+
+         {character.map(character => {
+          console.log(character);
+          return <Cards
             clickCount={this.clickCount}
+            name={character.name}
             id={character.id}
             key={character.id}
             image={character.image}
+            handleClick={this.handleClick}
           />
-        ))}
+         })} 
+        
+           </div>
+</div>
+{/* 
+{character.map(char => {
+  console.log(char);
+  let {name, image, id} = char;
+  return <Cards src={image} name={name} id={id} key={id}/>
+})} */}
       </Wrapper>
     );
   }
@@ -38,3 +92,4 @@ class App extends Component {
 
 
 export default App;
+
